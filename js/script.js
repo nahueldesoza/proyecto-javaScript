6dc33cccf6994d931a6479c98bbba401ead138ac
Nahuel de Soza <nahueldesoza@gmail.com>
2025-12-16 11:17:22 -0300

    agregado de funciones de muestras

--- js/script.js
+++ js/script.js
@@ -9,3 +9,25 @@ function ingreseEdad (){
     }
     return (edad);
 }
+function mostrar (nombre,edad){
+    while (edad < 0 || edad > 99) {
+        edad = prompt ("Valor de edad erroneo, por favor ingreselo nuevamente: ")
+    }
+    if (edad>=18){
+        alert("Hola "+nombre+", tenés "+edad+" años. Sos mayor.");
+    } else {
+        console.log("Hola "+nombre+", tenés "+edad+" años. Sos menor.");
+    }
+}
+let nombre = ingreseNombre ();
+let edad = ingreseEdad ();
+mostrar (nombre,edad);
+const arrayVacio=[]
+let a1 = prompt("Ingrese los nombres para el array (valor cero para salir):")
+while (a1 != 0){
+    arrayVacio.push(a1)
+    a1 = prompt("Ingrese los nombres para el array (valor cero para salir):")
+}
+for (i=0;i<arrayVacio.length;i++){
+    alert ("El nombre es: "+arrayVacio[i])
+}
